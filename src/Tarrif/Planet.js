0b099ae49e1f4ac114a1e8096d79a6fbb51f8330
Sudeep <mastersudeepkumar@gmail.com>
2023-10-09 23:51:28 +0530

    saved

--- src/Tarrif/Planet.js
+++ src/Tarrif/Planet.js
@@ -1,7 +1,20 @@
 import React from "react";
+import classes from "./Planet.module.css";
+import Back from "./Back.svg";
 
 const Planet = () => {
-  return <div>Hello</div>;
+  return (
+    <section>
+      <div>
+        <div className={classes.mainSection}>
+          <img src={Back} />
+          <p className={classes.mainP}>
+            Your EV Tariff <span className={classes.mainSpan}>quotes</span>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Planet;
